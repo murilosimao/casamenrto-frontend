98ae8bba165abbd7606b0e501210805b0014f692
Murilo Moreira Simão <50673829+murilosimao@users.noreply.github.com>
2024-06-15 11:56:33 -0300

    changes bg image

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import { util } from './util.js';
 import { like } from './like.js';
 import { theme } from './theme.js';
-import { audio } from './audio.js';
+// import { audio } from './audio.js';
 import { comment } from './comment.js';
 import { progress } from './progress.js';
 import { pagination } from './pagination.js';
@@ -9,7 +9,7 @@ import { pagination } from './pagination.js';
 window.util = util;
 window.like = like;
 window.theme = theme;
-window.audio = audio;
+// window.audio = audio;
 window.comment = comment;
 window.progress = progress;
 window.pagination = pagination;
